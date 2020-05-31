e316f1b5ed5239212c87f346fd14e6e39278825a
fimac <fimccawley@gmail.com>
2020-05-31 13:46:22 +1000

    Add js live view reqs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,20 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import "../css/app.css"
+import "../css/app.css";
+import { Socket } from "phoenix";
+import LiveSocket from "phoenix_live_view";
+
+let csrfToken = document
+  .querySelector("meta[name='csrf-token']")
+  .getAttribute("content");
+let liveSocket = new LiveSocket("/live", Socket, {
+  params: { _csrf_token: csrfToken },
+});
+
+liveSocket.connect();
+
+window.liveSocket = liveSocket;
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -12,4 +25,4 @@ import "../css/app.css"
 //     import {Socket} from "phoenix"
 //     import socket from "./socket"
 //
-import "phoenix_html"
+import "phoenix_html";
--- lib/gig_mems_v2_web/live/gig_search_live.ex
+++ lib/gig_mems_v2_web/live/gig_search_live.ex
@@ -2,21 +2,18 @@ defmodule GigMemsV2Web.GigSearchLive do
   use Phoenix.LiveView
   alias GigMemsV2Web.GigView
 
-  # alias GigMemsV2.GigApi
+  alias GigMemsV2.GigApi
 
   def render(assigns) do
     GigView.render("gig_search_live.html", assigns)
   end
 
   def mount(_params, _session, socket) do
-    IO.inspect(socket, label: "mounted")
     {:ok, assign(socket, gigs: [])}
   end
 
   def handle_event("search", %{"search_field" => %{"query" => query}}, socket) do
-    IO.inspect(query, label: "+++++++")
-    IO.puts("handle event")
-    # gigs = GigApi.get_gig(query)
-    {:noreply, socket}
+    gigs = GigApi.get_gig(query) |> GigApi.parse_response()
+    {:noreply, assign(socket, gigs: gigs)}
   end
 end
